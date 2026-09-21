1d8bb075a6b2ff5bb467646e83bf9971e9d47031
abdulrasheedshaikgit <abdulrasheedshaik8@gmail.com>
2026-09-21 22:17:56 +0530

    added basic auth

--- src/main/java/com/seciruty/SecurityDemo/SecurityConfig.java
+++ src/main/java/com/seciruty/SecurityDemo/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,7 +21,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-      http.authorizeHttpRequests(
+      http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(
               auth-> auth.requestMatchers("/authenticate").permitAll()
                       .anyRequest().authenticated())
               .httpBasic(Customizer.withDefaults());
