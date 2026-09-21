7443dd82a7fba2319f89056c2cc3b7146e129deb
abdulrasheedshaikgit <abdulrasheedshaik8@gmail.com>
2026-09-21 19:16:51 +0530

    added basic auth

--- src/main/java/com/seciruty/SecurityDemo/commandLiner/RunnerClass.java
+++ src/main/java/com/seciruty/SecurityDemo/commandLiner/RunnerClass.java
@@ -6,22 +6,23 @@ import com.seciruty.SecurityDemo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RunnerClass {
 
-    @Autowired
-    UserRepository repository;
-
     @Bean
-    public CommandLineRunner insertUsers() {
+    public CommandLineRunner insertUsers(UserRepository repository, PasswordEncoder passwordEncoder) {
         return args -> {
-            User user = new User();
-            user.setUsername("admin");
-            user.setPassword("admin1234");
-            user.setRole("ROLE_ADMIN");
-            repository.save(user);
+            if(repository.findUserByUsername("admin1").isEmpty()) {
+                User user = new User();
+                user.setUsername("admin1");
+                user.setPassword(passwordEncoder.encode("admin1234"));
+                user.setRole("ROLE_ADMIN");
+                repository.save(user);
+            }
         };
     }
 }
